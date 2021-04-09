db352a74002d5d27aa2f59057370a1d3e8fb6d25
Ilianix <49515712+Ilianix@users.noreply.github.com>
2021-04-09 13:17:40 +0200

    cambio de col-* en admin y user

--- 1broggiGrup3/resources/views/paginas/Admin/AdminMod.blade.php
+++ 1broggiGrup3/resources/views/paginas/Admin/AdminMod.blade.php
@@ -1,58 +1,58 @@
 @extends('index')
 @section('cuerpo')
-<div>
+
+<div class="container col-12">
+
     <div class="text-right">
-        <button type="button" class="btn btn-success" style="margin-right: 5%; margin-top: 5%;">Crear</button>
-</div>
-
-<div class="card border-primary mb-3" style="margin-top: 2%; margin-right: 5%; margin-left: 5%;">
-    <div class="card-body">
-        <table class="table table-hover">
-            <thead>
-            <tr class="table-primary">
-                <th scope="col">Type</th>
-                <th scope="col">Column heading</th>
-                <th scope="col">Column heading</th>
-                <th scope="col">Column heading</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr class="table-active">
-                <th scope="row">Active</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                <th><button type="button" class="btn btn-primary">Cambiar</button> | <button type="button" class="btn btn-danger">Borrar</button></th>
-            </tr>
-            <tr>
-                <th scope="row">Default</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                <th><button type="button" class="btn btn-primary">Cambiar</button> | <button type="button" class="btn btn-danger">Borrar</button></th>
-            </tr>
-            <tr>
-                <th scope="row">Default</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                <th><button type="button" class="btn btn-primary">Cambiar</button> | <button type="button" class="btn btn-danger">Borrar</button></th>
-            </tr>
-            <tr>
-                <th scope="row">Default</th>
-                <td>Column content</td>
-                <td>Column content</td>
-                <td>Column content</td>
-                <th><button type="button" class="btn btn-primary">Cambiar</button> | <button type="button" class="btn btn-danger">Borrar</button></th>
-            </tr>
-
-            </tbody>
-        </table>
+            <button type="button" class="btn btn-success" style="margin-right: 5%; margin-top: 5%;">Crear</button>
     </div>
-</div>
 
+    <div class="card border-primary col-12" style="margin-top: 2%; margin-right: 5%; margin-left: 5%;">
+        <div class="card-body col-12">
+            <table class="table table-hover col-12">
+                <thead>
+                <tr class="table-primary">
+                    <th scope="col">Type</th>
+                    <th scope="col">Column heading</th>
+                    <th scope="col">Column heading</th>
+                    <th scope="col">Column heading</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="table-active">
+                    <th scope="row">Active</th>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <th><button type="button" class="btn btn-primary">Cambiar</button> | <button type="button" class="btn btn-danger">Borrar</button></th>
+                </tr>
+                <tr>
+                    <th scope="row">Default</th>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <th><button type="button" class="btn btn-primary">Cambiar</button> | <button type="button" class="btn btn-danger">Borrar</button></th>
+                </tr>
+                <tr>
+                    <th scope="row">Default</th>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <th><button type="button" class="btn btn-primary">Cambiar</button> | <button type="button" class="btn btn-danger">Borrar</button></th>
+                </tr>
+                <tr>
+                    <th scope="row">Default</th>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <td>Column content</td>
+                    <th><button type="button" class="btn btn-primary">Cambiar</button> | <button type="button" class="btn btn-danger">Borrar</button></th>
+                </tr>
 
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 @endsection
--- 1broggiGrup3/resources/views/paginas/Admin/UserMod.blade.php
+++ 1broggiGrup3/resources/views/paginas/Admin/UserMod.blade.php
@@ -9,44 +9,44 @@
             <fieldset>
 
               <div class="form-group row">
-                <label for="exampleInputName1" class="col-sm-2 col-form-label">Nombre Usuario</label>
-                <div class="col-sm-10">
+                <label for="exampleInputName1" class="col-4 col-form-label">Nombre Usuario</label>
+                <div class="col-8">
                 <input type="email" class="form-control" id="exampleInputName1" aria-describedby="namelHelp" placeholder="Nombre usuario">
                 </div>
               </div>
 
               <div class="form-group row">
-                <label for="exampleInputPassw1" class="col-sm-2 col-form-label">Contraseña</label>
-                <div class="col-sm-10">
+                <label for="exampleInputPassw1" class="col-4 col-form-label">Contraseña</label>
+                <div class="col-8">
                 <input type="email" class="form-control" id="exampleInputPassw1" aria-describedby="passwHelp" placeholder="Contraseña">
                 </div>
               </div>
 
               <div class="form-group row">
-                <label for="staticEmail" class="col-sm-2 col-form-label">Correo</label>
-                <div class="col-sm-10">
+                <label for="staticEmail" class="col-4 col-form-label">Correo</label>
+                <div class="col-8">
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Correo">
                 </div>
               </div>
 
               <div class="form-group row">
-                <label for="exampleInputnamel1" class="col-sm-2 col-form-label">Nombre</label>
-                <div class="col-sm-10">
+                <label for="exampleInputnamel1" class="col-4 col-form-label">Nombre</label>
+                <div class="col-8">
                 <input type="email" class="form-control" id="exampleInputnamel1" aria-describedby="namelHelp" placeholder="Nombre">
                 </div>
               </div>
 
               <div class="form-group row">
-                <label for="exampleInputSubname" class="col-sm-2 col-form-label">Apellido</label>
-                <div class="col-sm-10">
+                <label for="exampleInputSubname" class="col-4 col-form-label">Apellido</label>
+                <div class="col-8">
                 <input type="email" class="form-control" id="exampleInputSubname" aria-describedby="subnamelHelp" placeholder="Apellido">
                 </div>
               </div>
 
 
               <div class="form-group row">
-                <label for="rol" class="col-sm-2 col-form-label">Rol</label>
-                <div class="col-sm-10">
+                <label for="rol" class="col-4 col-form-label">Rol</label>
+                <div class="col-8">
                 <select class="form-control" id="rol">
                   <option>1</option>
                   <option>2</option>
