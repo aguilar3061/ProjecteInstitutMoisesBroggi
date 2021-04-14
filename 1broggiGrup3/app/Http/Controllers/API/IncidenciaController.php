5c55928d896d1f60852200fa3615de71a4269e97
aguilar3061 <carlosaguilarher2000@gmail.com>
2021-04-14 14:11:27 +0200

    API incidencias pelada

---
+++ 1broggiGrup3/app/Http/Controllers/API/IncidenciaController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\IncidenciaResource;
+use App\Models\Incidencia;
+use Illuminate\Http\Request;
+
+class IncidenciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $incidencia = Incidencia::all();
+        return IncidenciaResource::collection($incidencia);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Incidencia  $incidencia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Incidencia $incidencia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Incidencia  $incidencia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Incidencia $incidencia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Incidencia  $incidencia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Incidencia $incidencia)
+    {
+        //
+    }
+}
