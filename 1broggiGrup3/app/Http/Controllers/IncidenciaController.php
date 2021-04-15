a341c8e4bbfdb503e42b2dbd17cb5e3028b06de9
horitzon1999 <56963133+horitzon1999@users.noreply.github.com>
2021-04-15 16:44:20 +0200

    madre mia que follon
    sfdsf

--- 1broggiGrup3/app/Http/Controllers/IncidenciaController.php
+++ 1broggiGrup3/app/Http/Controllers/IncidenciaController.php
@@ -6,7 +6,11 @@ use App\Models\Incidencia;
 use App\Models\Tipus_recurs;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Municipi;
+use App\Models\Provincia;
 use App\Models\Recurs;
+use App\Models\Tipus_alertant;
+use App\Models\Tipus_Incidencia;
 
 class IncidenciaController extends Controller
 {
@@ -17,10 +21,14 @@ class IncidenciaController extends Controller
      */
     public function index()
     {
-        $tipoRecursos = Tipus_recurs::all();
+        $tiporecursos = Tipus_recurs::all();
         $recursos = Recurs::all();
+        $provincias = Provincia::all();
+        $municipios = Municipi::all();
+        $tipusalertants = Tipus_alertant::all();
+        $tipusincidencies = Tipus_Incidencia::all();
+        return view('paginas.incidencias.indexVue', compact('recursos','tiporecursos','provincias','municipios','tipusalertants','tipusincidencies'));
 
-        return view('paginas.incidencias.index_vue', compact('tipoRecursos','recursos'));
     }
 
     /**
---
+++ 1broggiGrup3/resources/views/paginas/incidencias/indexVue.blade.php
@@ -0,0 +1,11 @@
+
+@extends('index')
+@section('cuerpo')
+@include('partials.mensajes')
+
+<div class="card border-primary mb-3 ">
+    <div class="card">
+        <incidencies-component :recursos="{{ $recursos }}" :tiporecursos="{{ $tiporecursos }}"  :provincias="{{ $provincias }}" :municipios="{{ $municipios }}" :tipusalertants="{{ $tipusalertants }}" :tipusincidencies="{{ $tipusincidencies }}"></incidencies-component>
+    </div>
+</div>
+@endsection
