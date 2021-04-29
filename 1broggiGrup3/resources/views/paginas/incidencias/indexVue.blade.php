e1e00ce7c4612b89163c9428d89c66a8acff615b
aguilar3061 <carlosaguilarher2000@gmail.com>
2021-04-29 19:32:46 +0200

    Update indexVue.blade.php

--- 1broggiGrup3/resources/views/paginas/incidencias/indexVue.blade.php
+++ 1broggiGrup3/resources/views/paginas/incidencias/indexVue.blade.php
@@ -5,7 +5,7 @@
 
 <div class="border-primary mb-3 ">
     <div >
-        <incidencies-component :recursos="{{ $recursos }}" :tiporecursos="{{ $tiporecursos }}" :provincias="{{ $provincias }}" :municipios="{{ $municipios }}" :tipusalertants="{{ $tipusalertants }}" :tipusincidencies="{{ $tipusincidencies }}" :alertants="{{ $alertants }}" ></incidencies-component>
+        <incidencies-component :afectats="{{ $afectats }}" :recursos="{{ $recursos }}" :tiporecursos="{{ $tiporecursos }}" :provincias="{{ $provincias }}" :municipios="{{ $municipios }}" :tipusalertants="{{ $tipusalertants }}" :tipusincidencies="{{ $tipusincidencies }}" :alertants="{{ $alertants }}" ></incidencies-component>
     </div>
 </div>
 @endsection
